8b7c8b16f56fc23aebe8b6052fe37855c1ea2869
KARA3O <203657323+KARA3O@users.noreply.github.com>
2026-07-31 13:43:25 +0200

    Initial Spring Boot setup with PostgreSQL connection

---
+++ src/main/java/com/karabo/taskflow/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.karabo.taskflow.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "TaskFlow API is running!";
+    }
+}
